30ac0e82a135412be5d35262c69283cd3d5c27bd
Ian Dunn <ian@iandunn.name>
2019-05-24 13:28:00 -0700

    Footer: move sponsors to content, add bg color

--- template-parts/front-page.php
+++ template-parts/front-page.php
@@ -39,6 +39,27 @@
 						</ul>
 					</div>
 				</section>
+
+				<!-- todo use Sponsors block instead of widgets
+				also, move this to a more appropriate place, like its own template part?-->
+				<div id="footer-widget-1" class="footer-widgets-block">
+					<section id="custom_html-5" class="widget_text widget widget_custom_html">
+						<h2 class="widget-title">Thank you sponsors</h2>
+						<div class="textwidget custom-html-widget"></div>
+					</section>
+					<section id="media_image-3" class="widget widget_media_image">
+						<img width="300" height="166" src="https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-300x166.png" class="image wp-image-22  attachment-medium size-medium" alt="" style="max-width: 100%; height: auto;" srcset="https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-300x166.png 300w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-768x426.png 768w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-1024x568.png 1024w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-500x277.png 500w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost.png 1038w" sizes="(max-width: 300px) 100vw, 300px" />
+					</section>
+					<section id="media_image-6" class="widget widget_media_image">
+						<img width="241" height="250" src="https://2019.seattle.wordcamp.test/files/2019/05/cropped-WP-SpaceNeedle-logo.png" class="image wp-image-107  attachment-full size-full" alt="" style="max-width: 100%; height: auto;" />
+					</section>
+					<section id="media_image-5" class="widget widget_media_image">
+						<img width="300" height="166" src="https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-300x166.png" class="image wp-image-22  attachment-medium size-medium" alt="" style="max-width: 100%; height: auto;" srcset="https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-300x166.png 300w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-768x426.png 768w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-1024x568.png 1024w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost-500x277.png 500w, https://2019.seattle.wordcamp.test/files/2019/05/dreamhost.png 1038w" sizes="(max-width: 300px) 100vw, 300px" />
+					</section>
+					<section id="media_image-4" class="widget widget_media_image">
+						<img width="241" height="250" src="https://2019.seattle.wordcamp.test/files/2019/05/cropped-WP-SpaceNeedle-logo.png" class="image wp-image-107  attachment-full size-full" alt="" style="max-width: 100%; height: auto;" />
+					</section>
+				</div>
 			</div>
 
 		</div>
