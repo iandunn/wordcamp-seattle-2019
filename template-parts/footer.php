ea53a7b831d5cd1b67c22cecf5a96552dd7b46c3
Ian Dunn <ian@iandunn.name>
2019-06-01 15:04:45 -0700

    Add footer content/markup

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -2,13 +2,147 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div id="footer-widgets">
 
+			<div id="footer-widget-1" class="footer-widgets-block">
+				<section id="nav_menu-4" class="widget widget_nav_menu">
+					<div class="menu-footer-container">
+						<ul id="menu-footer" class="menu">
+							<li id="menu-item-138" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-138">
+								<a href="https://2019.seattle.wordcamp.test/schedule/">Schedule</a>
 
+								<ul class="sub-menu">
+									<li id="menu-item-136" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-136">
+										<a href="https://2019.seattle.wordcamp.test/sessions/">Sessions</a></li>
+									<li id="menu-item-132" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-132">
+										<a href="https://2019.seattle.wordcamp.test/contact/">Contact</a></li>
+									<li id="menu-item-134" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-134">
+										<a href="https://2019.seattle.wordcamp.test/location/">Location</a></li>
+								</ul>
+							</li>
 
+							<li id="menu-item-133" class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children menu-item-133">
+								<a href="https://2019.seattle.wordcamp.test/organizers/">Organizers</a>
+								<ul class="sub-menu">
+									<li id="menu-item-139" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2 current_page_item menu-item-139">
+										<a href="https://2019.seattle.wordcamp.test/" aria-current="page">Sample Page</a>
+									</li>
+									<li id="menu-item-131" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-131">
+										<a href="https://2019.seattle.wordcamp.test/social-media-stream/">Social Media Stream</a>
+									</li>
+								</ul>
+							</li>
 
-			<!-- social media sites:
-			Facebook, Instagram, and Twitter
-			-->
+							<li id="menu-item-137" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-137">
+								<a href="https://2019.seattle.wordcamp.test/speakers/">Speakers</a></li>
+							<li id="menu-item-135" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-135">
+								<a href="https://2019.seattle.wordcamp.test/sponsors/">Sponsors</a></li>
+							<li id="menu-item-130" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-130">
+								<a href="https://2019.seattle.wordcamp.test/code-of-conduct/">Code of Conduct</a>
+							</li>
+						</ul>
+					</div>
+				</section>
 
+				<section id="jetpack_widget_social_icons-2" class="widget jetpack_widget_social_icons">
+					<h2 class="widget-title">Follow Us on Social Media</h2>
+					<ul class="jetpack-social-widget-list size-medium">
+						<li class="jetpack-social-widget-item">
+							<a href="https://facebook.com/wordcampseattle" target="_self">
+								<span class="screen-reader-text">Facebook</span>
+								<svg class="icon icon-facebook" aria-hidden="true" role="img">
+									<use href="#icon-facebook" xlink:href="#icon-facebook"></use>
+								</svg>
+							</a>
+						</li>
+
+						<li class="jetpack-social-widget-item">
+							<a href="https://twitter.com/wordcampseattle" target="_self">
+								<span class="screen-reader-text">Twitter</span>
+								<svg class="icon icon-twitter" aria-hidden="true" role="img">
+									<use href="#icon-twitter" xlink:href="#icon-twitter"></use>
+								</svg>
+							</a>
+						</li>
+
+						<li class="jetpack-social-widget-item">
+							<a href="https://instagram.com/wordcampseattle" target="_self">
+								<span class="screen-reader-text">Instagram</span>
+								<svg class="icon icon-instagram" aria-hidden="true" role="img">
+									<use href="#icon-instagram" xlink:href="#icon-instagram"></use>
+								</svg>
+							</a>
+						</li>
+					</ul>
+
+				</section>
+			</div>
+
+			<div id="footer-widget-2" class="footer-widgets-block">
+				<section id="blog_subscription-4" class="widget widget_blog_subscription jetpack_subscription_widget">
+					<h2 class="widget-title"></h2>
+					<form action="#" method="post" accept-charset="utf-8" id="subscribe-blog-blog_subscription-4">
+						<div id="subscribe-text">
+							<p>Sign-up Form for latest event updates</p>
+						</div>
+
+						<p id="subscribe-email">
+							<label id="jetpack-subscribe-label"
+								class="screen-reader-text"
+								for="subscribe-field-blog_subscription-4"> Email Address </label>
+							<input type="email" name="email" required="required" class="required"
+								value=""
+								id="subscribe-field-blog_subscription-4"
+								placeholder="Email Address" />
+						</p>
+
+						<p id="subscribe-submit">
+							<input type="hidden" name="action" value="subscribe" />
+							<input type="hidden" name="source" value="https://2017.testing.wordcamp.org/" />
+							<input type="hidden" name="sub-type" value="widget" />
+							<input type="hidden" name="redirect_fragment" value="blog_subscription-4" />
+							<input type="hidden" id="_wpnonce" name="_wpnonce" value="71b89ea70e" />
+							<button type="submit" name="jetpack_subscriptions_widget">
+								Let me know first!
+							</button>
+						</p>
+					</form>
+				</section>
+
+				<section id="custom_html-10" class="widget_text widget widget_custom_html">
+					<div class="textwidget custom-html-widget">
+						<a href="/tickets" class="button-link">Grab Tickets Now!</a>
+
+						<a class="site-info-network button-link" href="https://central.wordcamp.org/">Go to WordCamp Central</a>
+
+						<a class="site-info-generator" href="https://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator"> Proudly powered by WordPress </a>
+					</div>
+				</section>
+			</div>
+
+			<div id="footer-widget-3" class="footer-widgets-block">
+				<section id="custom_html-11" class="widget_text widget widget_custom_html">
+					<div class="textwidget custom-html-widget">
+						<a href="https://www.google.com/maps/dir//The+Conference+Center+at+the+Washington+State+Convention+Center,+800+Pike+St+%23804,+Seattle,+WA+98101/">
+							<img src="images/footer-map.png" alt="todo" /> </a>
+
+						<h4>
+							Nov. 12-13, 2019
+						</h4>
+						<a href="todo">Washington State Convention Center</a></div>
+				</section>
+			</div>
+
+		</div>
+
+		<div class="site-info">
+			<a class="site-info-generator" href="https://wordpress.org/" title="A Semantic Personal Publishing Platform" rel="generator"> Proudly powered by WordPress </a>
+
+			<span role="separator" aria-hidden="true"></span>
+
+			<a class="site-info-network" href="https://central.wordcamp.org/">Go to WordCamp Central</a>
+
+			<span role="separator" aria-hidden="true"></span>
+
+		</div>
 		</div>
 
 		<div class="site-info">
