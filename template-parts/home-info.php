2dd1c41943227c5ea82d0a7fa9df2dab05566d1a
Patty O'Hara <patty.ohara@gmail.com>
2019-06-03 22:19:15 -0700

    finish up info for you section

--- template-parts/home-info.php
+++ template-parts/home-info.php
@@ -1,46 +1,57 @@
-<h2>Info Just for You</h2>
-<div class="wp-block-columns has-2-columns info-block">
-	<div class="wp-block-column info-column info-column--attendees">
-		<h3 class="info-header">Attendees</h3>
-		<p>All you need to get started!</p>
-		<p><a href="?page=first-wordcamp-attendee">Is this your first WordCamp?</a></p>
-		<ul class="info-linklist">
-			<li><a href="?page=tickets">Get Tickets</a></li>
-			<li><a href="?page=badges">Badges</a></li>
-			<li><a href="?page=attendees">Attendee List</a></li>
-			<li><a href="?page=attendees-past">Past Attendees</a></li>
-		</ul>
-	</div>
-	<div class="wp-block-column info-column info-column--sponsors">
-		<h3 class="info-header">Sponsors</h3>
-		<p>Your support makes this possible</p>
-		<p><a href="/first-wordcamp-attendee">Is this your first WordCamp?</a></p>
-		<ul class="info-linklist">
-			<li><a href="http://tickets">Get Tickets</a></li>
-			<li><a href="/badges">Badges</a></li>
-			<li><a href="/attendees">Attendee List</a></li>
-			<li><a href="http://attendees-past">Past Attendees</a></li>
-		</ul>
-	</div>
-</div>
-<div class="wp-block-columns has-2-columns info-block">
-	<div class="wp-block-column info-column info-column--speakers">
-		<h3 class="info-header">Speakers</h3>
-		<p>Share your expertise as a user, developer, designer, business. Apply to speak.</p>
-		<ul class="info-linklist">
-			<li><a href="http://tickets">Apply to Speak</a></li>
-			<li><a href="/badges">Badges</a></li>
-			<li><a href="/attendees">Speakers List</a></li>
-			<li><a href="http://attendees-past">Topics</a></li>
-		</ul>
-	</div>
-	<div class="wp-block-column info-column info-column--volunteers">
-		<h3 class="info-header">Volunteers & Organizers</h3>
-		<p>We need your help! This is the best way to meet people, especially if you're shy.</p>
-		<ul class="info-linklist">
-			<li class="list-item-wide"><a href="http://tickets">Become a Volunteer</a></li>
-			<li><a href="/attendees">Organizers</a></li>
-			<li><a href="http://attendees-past">Badges</a></li>
-		</ul>
-	</div>
+<!-- INFO JUST FOR YOU SECTION -->
+<!-- HTML WIDGET -->
+<div id="content-widget-3" class="content-widgets-block">
+	<section id="custom_html-3" class="widget_text widget widget_custom_html">
+		<!-- WIDGET TITLE -->
+		<h2 class="widget-title">Info Just for You</h2>
+		<div class="textwidget custom-html-widget">
+		<!-- WIDGET CONTENT STARTS HERE -->
+			<div class="wp-block-columns has-2-columns info-block">
+				<div class="wp-block-column info-column info-column--attendees">
+					<h3 class="info-header">Attendees</h3>
+					<p>All you need to get started!</p>
+					<p><a href="?page=first-wordcamp-attendee">Is this your first WordCamp?</a></p>
+					<ul class="info-linklist">
+						<li><a href="?page=tickets">Get Tickets</a></li>
+						<li><a href="?page=badges">Badges</a></li>
+						<li><a href="?page=attendees">Attendee List</a></li>
+						<li><a href="?page=attendees-past">Past Attendees</a></li>
+					</ul>
+				</div>
+				<div class="wp-block-column info-column info-column--sponsors">
+					<h3 class="info-header">Sponsors</h3>
+					<p>Your support makes this possible</p>
+					<p><a href="/first-wordcamp-attendee">Is this your first WordCamp?</a></p>
+					<ul class="info-linklist">
+						<li><a href="http://tickets">Get Tickets</a></li>
+						<li><a href="/badges">Badges</a></li>
+						<li><a href="/attendees">Attendee List</a></li>
+						<li><a href="http://attendees-past">Past Attendees</a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="wp-block-columns has-2-columns info-block">
+				<div class="wp-block-column info-column info-column--speakers">
+					<h3 class="info-header">Speakers</h3>
+					<p>Share your expertise as a user, developer, designer, business. Apply to speak.</p>
+					<ul class="info-linklist">
+						<li><a href="http://tickets">Apply to Speak</a></li>
+						<li><a href="/badges">Badges</a></li>
+						<li><a href="/attendees">Speakers List</a></li>
+						<li><a href="http://attendees-past">Topics</a></li>
+					</ul>
+				</div>
+				<div class="wp-block-column info-column info-column--volunteers">
+					<h3 class="info-header">Volunteers &amp; Organizers</h3>
+					<p>We need your help! This is the best way to meet people, especially if you're shy.</p>
+					<ul class="info-linklist">
+						<li class="list-item-wide"><a href="http://tickets">Become a Volunteer</a></li>
+						<li><a href="/attendees">Organizers</a></li>
+						<li><a href="http://attendees-past">Badges</a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- WIDGET CONTENT ENDS ABOVE HERE -->
+		</div>
+	</section>
 </div>
