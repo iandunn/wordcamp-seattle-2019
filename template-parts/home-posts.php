b4aaded1ec46e620c8d587f943764a3329b0e2f6
pattyok <patty.ohara@gmail.com>
2019-06-03 22:25:01 -0700

    finish up info for you section (#10)

---
+++ template-parts/home-posts.php
@@ -0,0 +1,31 @@
+<!-- HOME PAGE Content Area 4 -->
+
+<div id="content-widget-4" class="content-widgets-block">
+	<!-- HTML widget with editable title -->
+	<section id="custom_html-10" class="widget_text widget widget_custom_html">
+		<h2 class="widget-title">Fuel Your Business</h2>
+		<div class="textwidget custom-html-widget"></div>
+	</section>
+	<!-- Jetpack display remote posts -->
+	<section class="widget jetpack_display_posts_widget-3 widget_jetpack_display_posts_widget">
+	<h3>Recent Posts: WordCamp Seattle 2019</h3>
+	<div class="jetpack-display-remote-posts">
+		<h4><a href="/just-testing/">Lorem ipsum dolor sit amet</a></h4>
+		<a title="Just testing" href="/just-testing/">
+			<img src="https://carkeekstudios.com/wp-content/uploads/2018/12/IMG_9682.jpg" alt="Just testing"/>
+		</a>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<h4><a href="/hello-world/">Duis aute irure dolor in reprehenderit</a></h4>
+		<a title="Just testing" href="/just-testing/">
+			<img src="https://carkeekstudios.com/wp-content/uploads/2018/12/IMG_9682.jpg" alt="Just testing"/>
+		</a>
+		<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<h4><a href="/hello-world/">Duis aute irure dolor in reprehenderit</a></h4>
+		<a title="Just testing" href="/just-testing/">
+			<img src="https://carkeekstudios.com/wp-content/uploads/2018/12/IMG_9682.jpg" alt="Just testing"/>
+		</a>
+		<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+	</div><!-- .jetpack-display-remote-posts -->
+	</section>
+</div>
