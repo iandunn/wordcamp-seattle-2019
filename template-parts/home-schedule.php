f21f1371648d7917173ee78b42f229f2fd0f5a0e
Patty O'Hara <patty.ohara@gmail.com>
2019-06-18 23:52:16 -0700

    added schedule section and made my sections responsive

---
+++ template-parts/home-schedule.php
@@ -0,0 +1,64 @@
+<!-- This varies from the comp after talking to Ryan - we wanted to get some 'what is wordcamp?'' text in here and not include the sponsor logo section-->
+
+<div id="content-widget-1" class="content-widgets-block">
+	<section id="media_image-3" class="widget widget_media_image">
+		<img width="640" height="480" src="images/wordcamp_2018.jpg" class="image wp-image-33  attachment-large size-large" alt=""  />
+	</section>
+	<section id="custom_html-20" class="widget_text widget widget_custom_html">
+
+		<div class="textwidget custom-html-widget">
+			<!-- HTML CONTENT START -->
+			<div class="wordcamp-wrapper">
+			<h2 class="widget-title">What is WordCamp?</h2>
+				<p>WordCamps are informal, community-organized events that focus on everything WordPress.</p>
+				<p>WordCamp Seattle brings together WordPress enthusiasts from around the Pacific Northwest for two days of education and networking. Whether you’re a developer, a designer, a consultant, an agency, a blogger, a publisher or end user, WordCamp has something for you.</p>
+				<!-- FULL TEXT FROM ROBERT - above was edited down some, thought we could put enough on the homepage and then link to the about page? -->
+				<!-- <p>WordCamp is a conference that focuses on everything WordPress.</p>
+				<p>WordCamps are informal, community-organized events put together by volunteer WordPress users like yourself. Everyone from casual users to core developers participates, shares ideas, and gets to know each other. ~ WordCamp Central</p>
+				<p>WordCamp Seattle brings together WordPress enthusiasts from around the Pacific Northwest for two days of education and networking. Whether you’re a developer, a designer, a consultant, an agency, a blogger, a publisher or end user, WordCamp has something for you. You&#8217;ll leave having learned a valuable skill, having reinforced what you know, and better connected.</p> -->
+			</div>
+		<!-- HTML CONTENT END -->
+		</div>
+	</section>
+	<section id="custom_html-18" class="widget_text widget widget_custom_html">
+		<div class="textwidget custom-html-widget">
+			<a href="/about" class="button arrow arrow-white button-large">Learn More About WordCamp Seattle</a>
+		</div>
+		</section>
+		<!-- another image here I think helps the layout feel more balanced-->
+		<section id="media_image-5" class="widget widget_media_image">
+			<img width="640" height="480" src="images/wordcamp_2018.jpg" class="image wp-image-33  attachment-large size-large" alt=""  />
+		</section>
+		<!-- RSS WIDGET set to the proper category, 1 posts at a time, include content -->
+	<section id="rss-4" class="widget widget_rss">
+		<h2 class="widget-title">
+			<a class="rsswidget" href="http://wordcamp-sea-2019.local/blog/category/schedule/feed/">
+				<img class="rss-widget-icon" style="border:0" width="14" height="14" src="http://wordcamp-sea-2019.local/wp-includes/images/rss.png" alt="RSS">
+			</a>
+			<a class="rsswidget" href="http://wordcamp-sea-2019.local/">Latest Schedule Updates</a>
+		</h2>
+		<ul>
+			<li>
+				<a class="rsswidget" href="http://wordcamp-sea-2019.local/blog/2019/06/04/another-sponsored-post/">Latest Schedule Post</a>
+				<div class="rssSummary">Pellentesque condimentum, sem faucibus blandit tincidunt, orci lorem sollicitudin ante, et sagittis libero urna sit amet eros. Pellentesque ut fringilla velit. Sed consectetur fermentum felis, at convallis dolor maximus nec. Quisque a vestibulum risus, in sollicitudin sem. Vivamus feugiat ac urna vel fringilla. Donec id lacus ut augue interdum ultrices ac id sem. Ut tristique […]</div>
+			</li>
+		</ul>
+	</section>
+		<section id="custom_html-16" class="widget_text widget widget_custom_html">
+			<div class="textwidget custom-html-widget">
+				<a href="/schedule" class="button arrow arrow-white button-large">View the Schedule</a>
+			</div>
+		</section>
+		<section id="nav_menu-6" class="widget widget_nav_menu">
+			<div class="menu-schedule-menu-container">
+				<ul id="menu-schedule-menu" class="menu">
+					<li id="menu-item-74" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-74"><a href="http://wordcamp-sea-2019.local/networking/">Networking</a></li>
+						<li id="menu-item-75" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-75"><a href="http://wordcamp-sea-2019.local/hallway-track/">Hallway Track</a></li>
+						<li id="menu-item-76" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-76"><a href="http://wordcamp-sea-2019.local/contributors/">Contributors</a></li>
+						<li id="menu-item-77" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-77"><a href="http://wordcamp-sea-2019.local/sponsored-activities/">Sponsored Activities</a></li>
+						<li id="menu-item-78" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-78"><a href="http://wordcamp-sea-2019.local/drinks-snacks-lunch/">Drinks, Snacks, Lunch</a></li>
+						<li id="menu-item-79" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-79"><a href="http://wordcamp-sea-2019.local/after-party/">After Party</a></li>
+						</ul>
+				</div>
+</section>
+</div>
