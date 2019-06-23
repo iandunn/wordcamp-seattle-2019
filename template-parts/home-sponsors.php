445e83dafa6d9432f081f764b8186e74c785c720
Ian Dunn <ian@iandunn.name>
2019-06-23 16:21:14 -0700

    Add sponsors section

---
+++ template-parts/home-sponsors.php
@@ -0,0 +1,61 @@
+<section id="wcb_sponsors-2" class="widget wcb_widget_sponsors">
+	<style>
+		.wcb_widget_sponsors .sponsor-logo {
+			display: block;
+			padding-bottom: 10px;
+		}
+
+		.wcb_widget_sponsors .sponsor-logo img {
+			max-width: 100%;
+			height: auto;
+
+			/* Conform to https://adblockplus.org/acceptable-ads */
+			max-height: 150px;
+			width: auto;
+		}
+	</style>
+
+	<div class="sponsor-level gold">
+		<h4 class="sponsor-level-title">Gold</h4>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/woocommerce/">
+			<img width="432" height="87" src="https://2019.seattle.wordcamp.org/files/2019/01/woocommerce_logo.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="WooCommerce" srcset="https://2019.seattle.wordcamp.org/files/2019/01/woocommerce_logo.png 432w, https://2019.seattle.wordcamp.org/files/2019/01/woocommerce_logo-300x60.png 300w" sizes="(max-width: 432px) 100vw, 432px" />
+		</a>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/jetpack/">
+			<img width="545" height="150" src="https://2019.seattle.wordcamp.org/files/2019/01/Jetpack_Horizontal_RGB-1.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="Jetpack" srcset="https://2019.seattle.wordcamp.org/files/2019/01/Jetpack_Horizontal_RGB-1.png 545w, https://2019.seattle.wordcamp.org/files/2019/01/Jetpack_Horizontal_RGB-1-300x83.png 300w, https://2019.seattle.wordcamp.org/files/2019/01/Jetpack_Horizontal_RGB-1-500x138.png 500w" sizes="(max-width: 545px) 100vw, 545px" />
+		</a>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/bluehost/">
+			<img width="651" height="137" src="https://2019.seattle.wordcamp.org/files/2019/01/bh_logo_2015.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="Bluehost" srcset="https://2019.seattle.wordcamp.org/files/2019/01/bh_logo_2015.png 651w, https://2019.seattle.wordcamp.org/files/2019/01/bh_logo_2015-300x63.png 300w, https://2019.seattle.wordcamp.org/files/2019/01/bh_logo_2015-500x105.png 500w" sizes="(max-width: 651px) 100vw, 651px" />
+		</a>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/godaddy/">
+			<img width="400" height="91" src="https://2019.seattle.wordcamp.org/files/2019/01/GoDaddy_Pro_RGB_Full_B2.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="GoDaddy Pro" srcset="https://2019.seattle.wordcamp.org/files/2019/01/GoDaddy_Pro_RGB_Full_B2.png 400w, https://2019.seattle.wordcamp.org/files/2019/01/GoDaddy_Pro_RGB_Full_B2-300x68.png 300w" sizes="(max-width: 400px) 100vw, 400px" />
+		</a>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/liquid-web/">
+			<img width="300" height="199" src="https://2019.seattle.wordcamp.org/files/2019/02/LW-Logo-Stacked_300w.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="Liquid Web™" />
+		</a>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/wordfence/">
+			<img width="1919" height="302" src="https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="" srcset="https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01.png 1919w, https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01-300x47.png 300w, https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01-768x121.png 768w, https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01-1024x161.png 1024w, https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01-500x79.png 500w, https://2019.seattle.wordcamp.org/files/2019/05/Wordfence-800Print-01-1568x247.png 1568w" sizes="(max-width: 1919px) 100vw, 1919px" />
+		</a>
+	</div><!-- .sponsor-level -->
+
+	<div class="sponsor-level silver">
+		<h4 class="sponsor-level-title">Silver</h4>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/greengeeks/">
+			<img width="505" height="100" src="https://2019.seattle.wordcamp.org/files/2019/02/2017-GreenGeeks-logo-CMYK-black-type.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="GreenGeeks® Web Hosting" srcset="https://2019.seattle.wordcamp.org/files/2019/02/2017-GreenGeeks-logo-CMYK-black-type.png 505w, https://2019.seattle.wordcamp.org/files/2019/02/2017-GreenGeeks-logo-CMYK-black-type-300x59.png 300w, https://2019.seattle.wordcamp.org/files/2019/02/2017-GreenGeeks-logo-CMYK-black-type-500x99.png 500w" sizes="(max-width: 505px) 100vw, 505px" />
+		</a>
+	</div><!-- .sponsor-level -->
+
+	<div class="sponsor-level bronze">
+		<h4 class="sponsor-level-title">Bronze</h4>
+
+		<a class="sponsor-logo" href="https://2019.seattle.wordcamp.org/sponsor/dreamhost/">
+			<img width="640" height="121" src="https://2019.seattle.wordcamp.org/files/2019/01/DreamHost-new-logo.png" class="attachment-wcb-sponsor-logo-horizontal-2x size-wcb-sponsor-logo-horizontal-2x wp-post-image" alt="DreamHost®" srcset="https://2019.seattle.wordcamp.org/files/2019/01/DreamHost-new-logo.png 640w, https://2019.seattle.wordcamp.org/files/2019/01/DreamHost-new-logo-300x57.png 300w, https://2019.seattle.wordcamp.org/files/2019/01/DreamHost-new-logo-500x95.png 500w" sizes="(max-width: 640px) 100vw, 640px" />
+		</a>
+	</div><!-- .sponsor-level -->
+</section>
