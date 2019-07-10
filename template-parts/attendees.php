0d484ee9cf3efcf6790434b413adca2d99b748d7
Ian Dunn <ian@iandunn.name>
2019-07-09 21:03:20 -0700

    Style Attendees page

--- template-parts/attendees.php
+++ template-parts/attendees.php
@@ -4,25 +4,61 @@
 	</header>
 
 	<div class="entry-content">
-		<p>This is an example page. It&#8217;s different from a blog post because it will stay in one place and will show up in your site navigation (in most themes). Most people start with an About page that introduces them to potential site visitors. It might say something like this:</p>
+		<div id="tix-attendees">
+			<ul class="tix-attendee-list tix-columns-3">
+				<li>
+					<img alt="" src="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=150&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=192&amp;d=mm&amp;r=g 2x" class="avatar avatar-96 photo" height="96" width="96" originals="96" src-orig="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=96&amp;d=mm&amp;r=g" scale="3">
 
+					<div class="tix-field tix-attendee-name">
+						<span class="tix-first">Cami</span> <span class="tix-last">MacNamara</span>
+					</div>
 
-		<blockquote class="wp-block-quote">
-			<p>Hi there! I&#8217;m a bike messenger by day, aspiring actor by night, and this is my website. I live in Los Angeles, have a great dog named Jack, and I like pi&#241;a coladas. (And gettin&#8217; caught in the rain.)</p>
-		</blockquote>
+					<a class="tix-field tix-attendee-twitter" href="http://twitter.com/webcami">@webcami</a><a class="tix-field tix-attendee-url" href="http://webcami.com">webcami.com</a>
+				</li>
 
+				<li>
+					<img alt="" src="https://secure.gravatar.com/avatar/f750acb8777eb6b7f76f088b25919804?s=150&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/f750acb8777eb6b7f76f088b25919804?s=192&amp;d=mm&amp;r=g 2x" class="avatar avatar-96 photo" height="96" width="96" originals="96" src-orig="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=96&amp;d=mm&amp;r=g" scale="3">
 
-		<p>&#8230;or something like this:</p>
+					<div class="tix-field tix-attendee-name">
+						<span class="tix-first">Merrill</span> <span class="tix-last">Mayer</span>
+					</div>
 
+					<a class="tix-field tix-attendee-twitter" href="http://twitter.com/koolkatweb">@koolkatweb</a><a class="tix-field tix-attendee-url" href="https://www.koolkatwebdesigns.com/">koolkatwebdesigns.com</a>
+				</li>
 
-		<blockquote class="wp-block-quote">
-			<p>The XYZ Doohickey Company was founded in 1971, and has been providing quality doohickeys to the public ever since. Located in Gotham City, XYZ employs over 2,000 people and does all kinds of awesome things for the Gotham community.</p>
-		</blockquote>
+				<li>
+					<img alt="" src="https://secure.gravatar.com/avatar/9ac84a9211c72a9a4a3c500d00126c94?s=150&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/9ac84a9211c72a9a4a3c500d00126c94?s=192&amp;d=mm&amp;r=g 2x" class="avatar avatar-96 photo" height="96" width="96" originals="96" src-orig="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=96&amp;d=mm&amp;r=g" scale="3">
 
+					<div class="tix-field tix-attendee-name">
+						<span class="tix-first">Rhonda</span> <span class="tix-last">Negard</span>
+					</div>
 
-		<p>As a new WordPress user, you should go to
-			<a href="https://2019.seattle.wordcamp.test/wp-admin/">your dashboard</a> to delete this page and create new pages for your content. Have fun!
-		</p>
+					<a class="tix-field tix-attendee-twitter" href="http://twitter.com/fatdogcreatives">@fatdogcreatives</a><a class="tix-field tix-attendee-url" href="https://www.fatdogcreatives.com">fatdogcreatives.com</a>
+				</li>
+
+				<li>
+					<img alt="" src="https://secure.gravatar.com/avatar/a883fd4f16d4ba6c8e741396877c91d7?s=150&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/a883fd4f16d4ba6c8e741396877c91d7?s=192&amp;d=mm&amp;r=g 2x" class="avatar avatar-96 photo" height="96" width="96" originals="96" src-orig="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=96&amp;d=mm&amp;r=g" scale="3">
+
+					<div class="tix-field tix-attendee-name">
+						<span class="tix-first">Robby</span> <span class="tix-last">McCullough</span>
+					</div>
+
+					<a class="tix-field tix-attendee-twitter" href="http://twitter.com/RobbyMcCullough">@RobbyMcCullough</a><a class="tix-field tix-attendee-url" href="https://www.wpbeaverbuilder.com">wpbeaverbuilder.com</a>
+				</li>
+
+				<li>
+					<img alt="" src="https://secure.gravatar.com/avatar/8889f26b4e29d79ba2e2a706d0f602fc?s=150&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/8889f26b4e29d79ba2e2a706d0f602fc?s=192&amp;d=mm&amp;r=g 2x" class="avatar avatar-96 photo" height="96" width="96" originals="96" src-orig="https://secure.gravatar.com/avatar/f0bd14a4e9c89b5788c6995c1c7b7106?s=96&amp;d=mm&amp;r=g" scale="3">
+
+					<div class="tix-field tix-attendee-name">
+						<span class="tix-first">Tim</span> <span class="tix-last">Godfrey</span>
+					</div>
+
+					<a class="tix-field tix-attendee-twitter" href="http://twitter.com/fuseiq">@fuseiq</a><a class="tix-field tix-attendee-url" href="https://fuseiq.com/">fuseiq.com</a>
+				</li>
+			</ul>
+		</div>
+
+        <br class="tix-clear" />
 	</div>
 
 	<footer class="entry-footer">
