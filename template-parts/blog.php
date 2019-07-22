3da104a7557746421035ecaf400b96b170b91485
Patty O'Hara <patty.ohara@gmail.com>
2019-07-22 14:52:53 -0700

    blog style updates

---
+++ template-parts/blog.php
@@ -0,0 +1,294 @@
+<main id="main" class="site-main" role="main">
+
+								<header>
+						<h1 class="page-title screen-reader-text">
+							Blog						</h1>
+					</header>
+
+
+<article id="post-17387" class="post-17387 post type-post status-publish format-standard has-post-thumbnail hentry category-announcements">
+	<header class="entry-header">
+		<h2 class="entry-title"><a href="https://2019.seattle.wordcamp.org/2019/07/10/call-for-speakers/" rel="bookmark">Call for Speakers</a></h2>			<div class="entry-meta">
+
+	<span class="posted-on">
+		Posted on
+		<a href="https://2019.seattle.wordcamp.org/2019/07/10/call-for-speakers/" rel="bookmark">
+
+				<time class="entry-date published" datetime="2019-07-10T00:14:53+00:00">
+					July 10, 2019				</time>
+
+				<time class="updated" datetime="2019-07-18T23:55:49+00:00">
+					July 18, 2019				</time>
+
+					</a>
+	</span>
+
+	<span class="byline">
+		by
+		<span class="author vcard">
+			<a class="url fn n" href="https://2019.seattle.wordcamp.org/author/rtvenge/">
+				rtvenge			</a>
+		</span>
+	</span>
+
+				</div>
+			</header>
+
+	<div class="entry-content">
+
+<h2>You (yes you!) are invited to apply to speak at WordCamp Seattle 2019!</h2>
+
+
+
+<p>Perhaps you have a passion for WordPress or you want to share your WordPress-related technological or business insights and expertise. </p>
+
+
+
+<p>Maybe you want to tell the story of how WordPress powers your dream, drives your business or transforms your professional or personal life. If so, why not apply to be a speaker?</p>
+
+
+
+<p>WordCamp Seattle is a 100% community-driven event, which means an opportunity for everyone to both teach and learn, listen and speak. Everyone has a story to tell and even if you haven’t considered it before, we encourage you to apply.</p>
+
+
+
+<p>Successful talks span a range of WordPress-related topics from development and design to open source, business, marketing, and more. We are looking for speakers who can teach, inform, and inspire.</p>
+
+
+
+<p>As a speaker, you’ll receive complementary access to the event including lunch. More importantly, you’ll join a community of developers, designers and leaders who come together to make WordCamp Seattle happen.</p>
+
+
+
+<h3>Speaking and application details</h3>
+
+
+
+<p>This year, we will be accepting applications for both full (max 45 minute) and lightning (max 10 minute) sessions.  In addition, if you would like to lead a more hands-on session, you can apply to lead a 2-hour workshop on any WordPress topic that will introduce participants to a new skill or technique.  Please indicate your preferred format when you apply.</p>
+
+
+
+<p>Please use the <a href="/apply-to-speak/">Apply to Speak form</a> to submit your application. The call for speakers closes at midnight PST on August 18th.</p>
+
+
+
+<p>You are welcome to submit multiple proposals, up to a maximum of three. If you submit more than three, we will consider only your first three submissions.</p>
+
+
+
+<p>Be sure your talk description/abstract defines the exact topic you want to talk about and what you hope people will learn or gain from attending your session.</p>
+
+
+
+<h3>How speakers are selected</h3>
+
+
+
+<p>A committee of volunteer organizers will review submissions beginning on August 19th. Applicants will be notified regarding the status of their application beginning on or around August 22nd. If you don’t hear from us immediately on the 22nd, please be patient as it may take some time to inform all applicants of their status. If you submit multiple proposals, you will only receive one response from us.</p>
+
+
+
+<div class="wp-block-button"><a class="wp-block-button__link" href="/apply-to-speak/">Apply To Speak</a></div>
+
+
+
+<p>WordCamp Seattle thrives as a result of many talented and inspiring speakers from many backgrounds. Even if you’ve never done anything like this before we invite you to <a href="/apply-to-speak/">apply</a> to join us as a speaker at WordCamp Seattle this year.</p>
+	</div>
+
+	<footer class="entry-footer">
+		<span class="cat-links">Posted in <a href="https://2019.seattle.wordcamp.org/category/announcements/">Announcements</a></span><span class="comments-link"><a href="https://2019.seattle.wordcamp.org/2019/07/10/call-for-speakers/#respond">Leave a Comment<span class="screen-reader-text"> on Call for Speakers</span></a></span><span class="edit-link"><a class="post-edit-link" href="https://2019.seattle.wordcamp.org/wp-admin/post.php?post=17387&amp;action=edit">Edit <span class="screen-reader-text">"Call for Speakers"</span></a></span>	</footer>
+</article>
+
+<article id="post-385" class="post-385 post type-post status-publish format-standard hentry category-announcements category-sponsors">
+	<header class="entry-header">
+		<h2 class="entry-title"><a href="https://2019.seattle.wordcamp.org/2019/04/30/call-for-sponsors/" rel="bookmark">Call for Sponsors</a></h2>			<div class="entry-meta">
+
+	<span class="posted-on">
+		Posted on
+		<a href="https://2019.seattle.wordcamp.org/2019/04/30/call-for-sponsors/" rel="bookmark">
+
+				<time class="entry-date published" datetime="2019-04-30T15:10:19+00:00">
+					April 30, 2019				</time>
+
+				<time class="updated" datetime="2019-05-09T04:23:46+00:00">
+					May 9, 2019				</time>
+
+					</a>
+	</span>
+
+	<span class="byline">
+		by
+		<span class="author vcard">
+			<a class="url fn n" href="https://2019.seattle.wordcamp.org/author/rtvenge/">
+				rtvenge			</a>
+		</span>
+	</span>
+
+				</div>
+			</header>
+
+	<div class="entry-content">
+
+<p>WordCamp Seattle is returning to the Washington State Conference Center on November 9th and 10th! A place for everyone involved within the WordPress ecosystem, from website developers to end users, this is an opportunity to reach this amazing, diverse community.</p>
+
+
+
+<p>Last year’s event brought on over 500 paid attendees, 39 speakers, 25 sponsors, and a few dozen volunteers to the conference. </p>
+
+
+
+<p>We have specifically designed the layout of the event space to create maximum visibility for our sponsors. Sponsor tables are set up in the main common area with the center serving as a hub for informal conversations, meals, and networking.</p>
+
+
+
+<p>With coffee, tea, and snacks served at the far end of the space, we’ve made sure every booth is highly visible and will see foot traffic through the conference. </p>
+
+
+
+<p>Our sponsor packages include an option to fit your budget, and we’d love to have you be part of our community!:</p>
+
+
+
+<h3>Packages</h3>
+
+
+
+<ul><li>Freelancer ($100)</li><li>Small Business ($250)</li><li>Bronze ($1,000)</li><li>Silver, ($2,500)</li><li>Gold ($5,000). </li></ul>
+
+
+
+<p>For more information about becoming a sponsor and what each level includes, please <a href="https://2019.seattle.wordcamp.org/sponsors/sponsorship/">visit our Sponsorship page</a>.</p>
+	</div>
+
+	<footer class="entry-footer">
+		<span class="cat-links">Posted in <a href="https://2019.seattle.wordcamp.org/category/announcements/">Announcements</a>, <a href="https://2019.seattle.wordcamp.org/category/sponsors/">Sponsors</a></span><span class="edit-link"><a class="post-edit-link" href="https://2019.seattle.wordcamp.org/wp-admin/post.php?post=385&amp;action=edit">Edit <span class="screen-reader-text">"Call for Sponsors"</span></a></span>	</footer>
+</article>
+
+<article id="post-182" class="post-182 post type-post status-publish format-standard has-post-thumbnail hentry category-volunteering">
+	<header class="entry-header">
+		<h2 class="entry-title"><a href="https://2019.seattle.wordcamp.org/2019/02/05/call-for-wordcamp-wranglers/" rel="bookmark">Call for WordCamp Wranglers</a></h2>			<div class="entry-meta">
+
+	<span class="posted-on">
+		Posted on
+		<a href="https://2019.seattle.wordcamp.org/2019/02/05/call-for-wordcamp-wranglers/" rel="bookmark">
+
+				<time class="entry-date published" datetime="2019-02-05T18:53:57+00:00">
+					February 5, 2019				</time>
+
+				<time class="updated" datetime="2019-06-27T00:31:57+00:00">
+					June 27, 2019				</time>
+
+					</a>
+	</span>
+
+	<span class="byline">
+		by
+		<span class="author vcard">
+			<a class="url fn n" href="https://2019.seattle.wordcamp.org/author/rtvenge/">
+				rtvenge			</a>
+		</span>
+	</span>
+
+				</div>
+			</header>
+
+	<div class="entry-content">
+
+<h3>Ladies &amp; Gentlemen and Previous WordCamp Organizers!</h3>
+
+
+
+<p><font color="#191e23"><span style="font-weight: 600">Positions Are Still Open</span></font></p>
+
+
+
+<p>We are still looking for people for the following positions:</p>
+
+
+
+<ul><li>Sponsor</li><li>Registration</li><li>AV</li></ul>
+
+
+
+<p>Vanessa, Ryan and Noah are excited to work with anyone who might be interested in joining our team. Please don’t hesitate to <a href="https://2019.seattle.wordcamp.org/contact/">ask questions</a> if you have them. We look forward to working with all of you!</p>
+
+
+
+<p><strong>Please fill out the </strong><a href="https://docs.google.com/forms/d/e/1FAIpQLSeGO0kz3a0EPYg3o7Vu8wq8dEI-VE2g_vF4Asc_x4AvjOyEYg/viewform"><strong>Interest Form</strong></a> if you are interested in joining our team.<br></p>
+	</div>
+
+	<footer class="entry-footer">
+		<span class="cat-links">Posted in <a href="https://2019.seattle.wordcamp.org/category/volunteering/">Volunteering</a></span><span class="edit-link"><a class="post-edit-link" href="https://2019.seattle.wordcamp.org/wp-admin/post.php?post=182&amp;action=edit">Edit <span class="screen-reader-text">"Call for WordCamp Wranglers"</span></a></span>	</footer>
+</article>
+
+<article id="post-3" class="post-3 post type-post status-publish format-standard has-post-thumbnail hentry category-announcements">
+	<header class="entry-header">
+		<h2 class="entry-title"><a href="https://2019.seattle.wordcamp.org/2019/01/04/welcome-to-wordcamp-seattle-wa-usa/" rel="bookmark">SAVE THE DATE: WordCamp Seattle: November 9th – 10th, 2019!</a></h2>			<div class="entry-meta">
+
+	<span class="posted-on">
+		Posted on
+		<a href="https://2019.seattle.wordcamp.org/2019/01/04/welcome-to-wordcamp-seattle-wa-usa/" rel="bookmark">
+
+				<time class="entry-date published" datetime="2019-01-04T21:31:13+00:00">
+					January 4, 2019				</time>
+
+				<time class="updated" datetime="2019-06-26T04:06:50+00:00">
+					June 26, 2019				</time>
+
+					</a>
+	</span>
+
+	<span class="byline">
+		by
+		<span class="author vcard">
+			<a class="url fn n" href="https://2019.seattle.wordcamp.org/author/rtvenge/">
+				rtvenge			</a>
+		</span>
+	</span>
+
+				</div>
+			</header>
+
+	<div class="entry-content">
+		<p>We’re happy to announce that WordCamp Seattle is officially on the calendar!</p>
+<p>WordCamp Seattle&nbsp;will be November 9th – 10th, 2019 at:</p>
+<p><strong>Conference Center at the<br>Washington State Convention Center<br></strong>705 Pike Street<br>Seattle, WA 98101-2310.</p>
+<p>Subscribe using the form below to stay up to date on the most recent news. We’ll be keeping you posted on all the details over the coming months, including speaker submissions, ticket sales and more!</p>
+
+<div id="contact-form-3">
+<form action="https://2019.seattle.wordcamp.org/2019/01/04/welcome-to-wordcamp-seattle-wa-usa/#contact-form-3" method="post" class="contact-form commentsblock">
+
+
+<div class="grunion-field-wrap grunion-field-name-wrap">
+<label for="g3-name" class="grunion-field-label name">Name<span>(required)</span></label>
+<input type="text" name="g3-name" id="g3-name" value="" class="name" required="" aria-required="true">
+	</div>
+
+
+
+<div class="grunion-field-wrap grunion-field-email-wrap">
+<label for="g3-email" class="grunion-field-label email">Email<span>(required)</span></label>
+<input type="email" name="g3-email" id="g3-email" value="" class="email" required="" aria-required="true">
+	</div>
+
+
+
+<div class="grunion-field-wrap grunion-field-textarea-wrap">
+<label for="contact-form-comment-g3-message" class="grunion-field-label textarea">Message</label>
+<textarea name="g3-message" id="contact-form-comment-g3-message" rows="20" class="textarea"></textarea>
+	</div>
+
+	<p class="contact-submit">
+		<button type="submit" class="pushbutton-wide">Submit</button>		<input type="hidden" id="_wpnonce" name="_wpnonce" value="1584d57316"><input type="hidden" name="_wp_http_referer" value="/blog/">
+		<input type="hidden" name="contact-form-id" value="3">
+		<input type="hidden" name="action" value="grunion-contact-form">
+		<input type="hidden" name="contact-form-hash" value="89dd25e1c2ea444774b5d3b3754150570e8c3550">
+	</p>
+</form>
+</div>	</div>
+
+	<footer class="entry-footer">
+		<span class="cat-links">Posted in <a href="https://2019.seattle.wordcamp.org/category/announcements/">Announcements</a></span><span class="edit-link"><a class="post-edit-link" href="https://2019.seattle.wordcamp.org/wp-admin/post.php?post=3&amp;action=edit">Edit <span class="screen-reader-text">"SAVE THE DATE: WordCamp Seattle: November 9th – 10th, 2019!"</span></a></span>	</footer>
+</article>
+
+		</main>
---
+++ template-parts/single-post.php
@@ -0,0 +1,110 @@
+<main id="main" class="site-main" role="main">
+
+<article id="post-17387" class="post-17387 post type-post status-publish format-standard has-post-thumbnail hentry category-announcements">
+	<header class="entry-header" style="display:block">
+		<h1 class="entry-title">Call for Speakers</h1>			<div class="entry-meta">
+
+	<span class="posted-on">
+		Posted on
+		<a href="https://2019.seattle.wordcamp.org/2019/07/10/call-for-speakers/" rel="bookmark">
+
+				<time class="entry-date published" datetime="2019-07-10T00:14:53+00:00">
+					July 10, 2019				</time>
+
+				<time class="updated" datetime="2019-07-18T23:55:49+00:00">
+					July 18, 2019				</time>
+
+					</a>
+	</span>
+
+	<span class="byline">
+		by
+		<span class="author vcard">
+			<a class="url fn n" href="https://2019.seattle.wordcamp.org/author/rtvenge/">
+				rtvenge			</a>
+		</span>
+	</span>
+
+				</div>
+			</header>
+
+	<div class="entry-content">
+
+<h2>You (yes you!) are invited to apply to speak at WordCamp Seattle 2019!</h2>
+
+
+
+<p>Perhaps you have a passion for WordPress or you want to share your WordPress-related technological or business insights and expertise. </p>
+
+
+
+<p>Maybe you want to tell the story of how WordPress powers your dream, drives your business or transforms your professional or personal life. If so, why not apply to be a speaker?</p>
+
+
+
+<p>WordCamp Seattle is a 100% community-driven event, which means an opportunity for everyone to both teach and learn, listen and speak. Everyone has a story to tell and even if you haven’t considered it before, we encourage you to apply.</p>
+
+
+
+<p>Successful talks span a range of WordPress-related topics from development and design to open source, business, marketing, and more. We are looking for speakers who can teach, inform, and inspire.</p>
+
+
+
+<p>As a speaker, you’ll receive complementary access to the event including lunch. More importantly, you’ll join a community of developers, designers and leaders who come together to make WordCamp Seattle happen.</p>
+
+
+
+<h3>Speaking and application details</h3>
+
+
+
+<p>This year, we will be accepting applications for both full (max 45 minute) and lightning (max 10 minute) sessions.  In addition, if you would like to lead a more hands-on session, you can apply to lead a 2-hour workshop on any WordPress topic that will introduce participants to a new skill or technique.  Please indicate your preferred format when you apply.</p>
+
+
+
+<p>Please use the <a href="/apply-to-speak/">Apply to Speak form</a> to submit your application. The call for speakers closes at midnight PST on August 18th.</p>
+
+
+
+<p>You are welcome to submit multiple proposals, up to a maximum of three. If you submit more than three, we will consider only your first three submissions.</p>
+
+
+
+<p>Be sure your talk description/abstract defines the exact topic you want to talk about and what you hope people will learn or gain from attending your session.</p>
+
+
+
+<h3>How speakers are selected</h3>
+
+
+
+<p>A committee of volunteer organizers will review submissions beginning on August 19th. Applicants will be notified regarding the status of their application beginning on or around August 22nd. If you don’t hear from us immediately on the 22nd, please be patient as it may take some time to inform all applicants of their status. If you submit multiple proposals, you will only receive one response from us.</p>
+
+
+
+<div class="wp-block-button"><a class="wp-block-button__link" href="/apply-to-speak/">Apply To Speak</a></div>
+
+
+
+<p>WordCamp Seattle thrives as a result of many talented and inspiring speakers from many backgrounds. Even if you’ve never done anything like this before we invite you to <a href="/apply-to-speak/">apply</a> to join us as a speaker at WordCamp Seattle this year.</p>
+	</div>
+
+	<footer class="entry-footer">
+		<span class="cat-links">Posted in <a href="https://2019.seattle.wordcamp.org/category/announcements/">Announcements</a></span><span class="edit-link"><a class="post-edit-link" href="https://2019.seattle.wordcamp.org/wp-admin/post.php?post=17387&amp;action=edit">Edit <span class="screen-reader-text">"Call for Speakers"</span></a></span>	</footer>
+</article>
+
+	<nav class="navigation post-navigation" role="navigation">
+		<h2 class="screen-reader-text">Post navigation</h2>
+		<div class="nav-links"><div class="nav-previous"><a href="https://2019.seattle.wordcamp.org/2019/04/30/call-for-sponsors/" rel="prev">Call for Sponsors</a></div></div>
+	</nav>
+<div id="comments" class="comments-area">
+
+		<div id="respond" class="comment-respond">
+		<h3 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow" id="cancel-comment-reply-link" href="/2019/07/10/call-for-speakers/#respond" style="display:none;">Cancel reply</a></small></h3>			<form action="https://2019.seattle.wordcamp.org/wp-comments-post.php" method="post" id="commentform" class="comment-form" novalidate="">
+				<p class="logged-in-as"><a href="https://2019.seattle.wordcamp.org/wp-admin/profile.php" aria-label="Logged in as pattyok. Edit your profile.">Logged in as pattyok</a>. <a href="https://2019.seattle.wordcamp.org/wp-login.php?action=logout&amp;redirect_to=https%3A%2F%2F2019.seattle.wordcamp.org%2F2019%2F07%2F10%2Fcall-for-speakers%2F&amp;_wpnonce=52c4305643">Log out?</a></p><p class="comment-form-comment"><label for="comment">Comment</label> <textarea id="comment" name="comment" cols="45" rows="8" maxlength="65525" required="required"></textarea></p><p class="comment-subscription-form"><input type="checkbox" name="subscribe_comments" id="subscribe_comments" value="subscribe" style="width: auto; -moz-appearance: checkbox; -webkit-appearance: checkbox;"> <label class="subscribe-label" id="subscribe-label" for="subscribe_comments">Notify me of follow-up comments by email.</label></p><p class="comment-subscription-form"><input type="checkbox" name="subscribe_blog" id="subscribe_blog" value="subscribe" style="width: auto; -moz-appearance: checkbox; -webkit-appearance: checkbox;"> <label class="subscribe-label" id="subscribe-blog-label" for="subscribe_blog">Notify me of new posts by email.</label></p><p class="form-submit"><input name="submit" type="submit" id="submit" class="submit" value="Post Comment"> <input type="hidden" name="comment_post_ID" value="17387" id="comment_post_ID">
+<input type="hidden" name="comment_parent" id="comment_parent" value="0">
+</p><p style="display: none;"><input type="hidden" id="akismet_comment_nonce" name="akismet_comment_nonce" value="95416a9b52"></p><p style="display: none;"></p>			<input type="hidden" id="ak_js" name="ak_js" value="1563827512772"></form>
+			</div><!-- #respond -->
+
+</div>
+		</main>
