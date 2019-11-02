96ef9fcee0c096d37a9982d513dcd73dd751771d
pattyok <patty.ohara@gmail.com>
2019-11-02 12:00:45 -0700

    props styling

---
+++ template-parts/props.php
@@ -0,0 +1,142 @@
+<style type="text/css">
+    .contact-form label {
+        margin-bottom: .25em;
+        float: none;
+        font-weight: 700;
+        display: block;
+    }
+</style>
+
+<div id="content" class="site-content">
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+
+<article id="post-18554" class="post-18554 page type-page status-publish hentry">
+	<header class="entry-header">
+
+		<h1 class="entry-title">Props</h1>	</header>
+
+	<div class="entry-content">
+
+<p>Give props to the speakers!</p>
+
+
+<div id='contact-form-18554'>
+<form action='https://2019.seattle.wordcamp.org/props/#contact-form-18554' method='post' class='contact-form commentsblock'>
+
+
+<div class='grunion-field-wrap grunion-field-email-wrap'  >
+<label
+				for='g18554-emailoptionalorifyouhaveaquestion'
+				class='grunion-field-label email'
+				>Email (Optional or if you have a question)</label>
+<input
+					type='email'
+					name='g18554-emailoptionalorifyouhaveaquestion'
+					id='g18554-emailoptionalorifyouhaveaquestion'
+					value=''
+					class='email'
+
+				/>
+	</div>
+
+
+
+<div class='grunion-field-wrap grunion-field-radio-wrap emoji-radio-buttons-wrap'  >
+<label
+				for='g18554-chooseone'
+				class='grunion-field-label'
+				>Choose one</label>
+		<label class='grunion-radio-label radio'><input
+									type='radio'
+									name='g18554-chooseone'
+									value='100' class='radio emoji-radio-buttons'  /> 100</label>
+		<div class='clear-form'></div>
+		<label class='grunion-radio-label radio'><input
+									type='radio'
+									name='g18554-chooseone'
+									value='Heart' class='radio emoji-radio-buttons'  /> Heart</label>
+		<div class='clear-form'></div>
+		<label class='grunion-radio-label radio'><input
+									type='radio'
+									name='g18554-chooseone'
+									value='Clap' class='radio emoji-radio-buttons'  /> Clap</label>
+		<div class='clear-form'></div>
+		<label class='grunion-radio-label radio'><input
+									type='radio'
+									name='g18554-chooseone'
+									value='Thumbs Up' class='radio emoji-radio-buttons'  /> Thumbs Up</label>
+		<div class='clear-form'></div>
+		<label class='grunion-radio-label radio'><input
+									type='radio'
+									name='g18554-chooseone'
+									value='Laugh Tears' class='radio emoji-radio-buttons'  /> Laugh Tears</label>
+		<div class='clear-form'></div>
+		<label class='grunion-radio-label radio'><input
+									type='radio'
+									name='g18554-chooseone'
+									value='Happy' class='radio emoji-radio-buttons'  /> Happy</label>
+		<div class='clear-form'></div>
+	</div>
+
+
+
+<div class='grunion-field-wrap grunion-field-select-wrap'  >
+<label
+				for='g18554-saturdayspeakersnametalk'
+				class='grunion-field-label select'
+				>Saturday Speakers Name/Talk</label>
+	<select name='g18554-saturdayspeakersnametalk' id='g18554-saturdayspeakersnametalk' class='select' >
+		<option value='Saturday'>Saturday</option>
+		<option value='Sat. 9:00 AM Jocelyn Mozak (Keynote)'>Sat. 9:00 AM Jocelyn Mozak (Keynote)</option>
+		<option value='Sat. 10:30 AM Greg Beuno  (Will you Like Your Code in Six Months)'>Sat. 10:30 AM Greg Beuno  (Will you Like Your Code in Six Months)</option>
+		<option value='Sat. 10:30 AM Aaron D. Campbell (Myths and Facts About Securing Your Site)'>Sat. 10:30 AM Aaron D. Campbell (Myths and Facts About Securing Your Site)</option>
+		<option value='Sat. 10:30 AM Nathan Ingram (Dealing with Problem Clients...)'>Sat. 10:30 AM Nathan Ingram (Dealing with Problem Clients&#8230;)</option>
+		<option value='Sat. 11:30 AM Patty O&#039;Hara (Helpful tools to automate clean'>Sat. 11:30 AM Patty O&#039;Hara (Helpful tools to automate clean</option>
+		<option value='maintainable code)'>maintainable code)</option>
+		<option value='Sat. 11:30 AM Kathy Zant (The Hacking Mindset...)'>Sat. 11:30 AM Kathy Zant (The Hacking Mindset&#8230;)</option>
+		<option value='Sat. 11:30 AM Bob Dunn (Build Your Brand: Podcasting with WordPress)'>Sat. 11:30 AM Bob Dunn (Build Your Brand: Podcasting with WordPress)</option>
+		<option value='Sat. 1:30 PM Chip Edwards (What does your brand look like in a voicefirst world?)'>Sat. 1:30 PM Chip Edwards (What does your brand look like in a voicefirst world?)</option>
+		<option value='Sat. 1:30 PM Kelli Wise (Keeping your content accessible)'>Sat. 1:30 PM Kelli Wise (Keeping your content accessible)</option>
+		<option value='Sat. 1:30 PM Rob Taylor (Content Planning - business as a blog'>Sat. 1:30 PM Rob Taylor (Content Planning &#8211; business as a blog</option>
+		<option value='blog as a business)'>blog as a business)</option>
+	</select>
+	</div>
+
+
+
+<div class='grunion-field-wrap grunion-field-textarea-wrap'  >
+<label
+				for='contact-form-comment-g18554-message'
+				class='grunion-field-label textarea'
+				>Message</label>
+<textarea
+		                name='g18554-message'
+		                id='contact-form-comment-g18554-message'
+		                rows='20' class='textarea'  ></textarea>
+	</div>
+
+	<p class='contact-submit'>
+		<button type='submit' class='pushbutton-wide'>Submit</button>		<input type="hidden" id="_wpnonce" name="_wpnonce" value="a84d77e9b0" /><input type="hidden" name="_wp_http_referer" value="/props/" />
+		<input type='hidden' name='contact-form-id' value='18554' />
+		<input type='hidden' name='action' value='grunion-contact-form' />
+		<input type='hidden' name='contact-form-hash' value='6da5d05a8c4ac2ea77edd6705d270525aac8a058' />
+	</p>
+</form>
+</div>
+
+
+<p>Form coming soon.…</p>
+	</div>
+
+			<footer class="entry-footer">
+			<span class="edit-link"><a class="post-edit-link" href="https://2019.seattle.wordcamp.org/wp-admin/post.php?post=18554&#038;action=edit">Edit <span class="screen-reader-text">"Props"</span></a></span>		</footer>
+	</article>
+
+		</main>
+	</div>
+
+
+		</div> <!-- #content -->
